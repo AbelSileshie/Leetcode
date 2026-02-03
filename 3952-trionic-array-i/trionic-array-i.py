b51b39ea8136249732563f7181b402009d3434ff
Abel S <150824348+AbelSileshie@users.noreply.github.com>
2026-02-03 21:09:43 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (55.16%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.py
+++ 3952-trionic-array-i/trionic-array-i.py
@@ -2,7 +2,6 @@ class Solution:
     def isTrionic(self, nums: List[int]) -> bool:
         n = len(nums)
         i = 1
-
         while i < n and nums[i - 1] < nums[i]:
             i += 1
         p = i - 1
@@ -14,10 +13,11 @@ class Solution:
         while i < n and nums[i - 1] < nums[i]:
             i += 1
         flag = i - 1
-
         return (
             p != 0 and
             q != p and
             flag == n - 1 and
             flag != q
         )
+
+ 
