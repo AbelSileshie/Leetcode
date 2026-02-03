031fed96e1026bb648cbcd8eae66e530aa748a73
Abel S <150824348+AbelSileshie@users.noreply.github.com>
2026-02-03 23:33:23 +0300

    Time: 823 ms (52.78%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,8 +1,4 @@
-DELETE FROM Person
-WHERE id NOT IN (
-    SELECT id FROM (
-        SELECT MIN(id) AS id
-        FROM Person
-        GROUP BY email
-    ) AS temp
-);
+DELETE p1 
+FROM Person p1, Person p2 
+WHERE p1.email = p2.email 
+AND p1.id > p2.id;
