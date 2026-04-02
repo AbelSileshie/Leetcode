7eb276ff54fada9dfa28d1a30229dfa8849d8b52
Abel S <150824348+AbelSileshie@users.noreply.github.com>
2026-04-02 14:55:22 +0300

    Time: 261 ms (64.14%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,4 @@
 SELECT (
-    SELECT DISTINCT salary
-    FROM Employee
-    ORDER BY salary DESC
-    LIMIT 1 OFFSET 1
-) AS SecondHighestSalary;
+    SELECT DISTINCT salary FROM employee ORDER BY salary DESC LIMIT 1 OFFSET 1
+)
+AS SecondHighestSalary;
